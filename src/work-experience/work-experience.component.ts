f189f60e87edf28cf26bb08fc9679579331dab7a
Yousef-Mohsen-Dev <usef99550@gmail.com>
2024-10-05 22:51:51 +0300

    add Animate.css library in the project and try it

--- src/work-experience/work-experience.component.ts
+++ src/work-experience/work-experience.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-work-experience',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './work-experience.component.html',
-  styleUrl: './work-experience.component.css'
+  styleUrl: './work-experience.component.css',
 })
 export class WorkExperienceComponent {
+  isBouncing = false;
 
+  toggleBounce() {
+    this.isBouncing = true;
+    setTimeout(() => {
+      this.isBouncing = false;
+    }, 1000); // Duration matches the animation time
+  }
 }
