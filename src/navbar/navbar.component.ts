cb77df8b67736fe16453b1304cb9fc7eaeaf2181
toyotadriver <polaemil@gmail.com>
2024-10-11 13:43:57 +0300

    "fixed navbar locking issue"

--- src/navbar/navbar.component.ts
+++ src/navbar/navbar.component.ts
@@ -1,36 +1,33 @@
 import { Component, HostListener } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { gsap, TweenMax,Power4,Power1, TweenLite,Expo,} from 'gsap';
+import { gsap, TweenMax, Power4, Power1, TweenLite, Expo } from 'gsap';
 import { delay } from 'rxjs';
-
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
-
   screenWidth = window.innerWidth;
 
-  constructor(){}
+  constructor() {}
   ngOnInit() {
-    let navbar = document.querySelector('#navbar')
+    let navbar = document.querySelector('#navbar');
     if (this.screenWidth <= 755) {
       navbar?.classList.add('mobile');
     } else {
       navbar?.classList.remove('mobile');
       navbar?.classList.remove('active');
     }
-    
   }
-  @HostListener ('window:resize') changeNavBar() {
+  @HostListener('window:resize') changeNavBar() {
     this.screenWidth = window.innerWidth;
-    let scroll = window.scrollY
-    let navbar = document.querySelector('#navbar')
+    let scroll = window.scrollY;
+    let navbar = document.querySelector('#navbar');
     if (this.screenWidth <= 755) {
       navbar?.classList.add('mobile');
     } else {
@@ -39,55 +36,51 @@ export class NavbarComponent {
     }
   }
 
-  showNavBar () {
-      let navbar = document.querySelector('#navbar') as HTMLHtmlElement;
-      let main = document.querySelector('main')
-      let body = document.querySelector('body')
-      let mobile = document.querySelector('#navbar.mobile') as HTMLHtmlElement;
-      navbar?.classList.toggle('active');
+  showNavBar() {
+    let navbar = document.querySelector('#navbar') as HTMLHtmlElement;
+    let main = document.querySelector('main');
+    let body = document.querySelector('body');
+    let mobile = document.querySelector('#navbar.mobile') as HTMLHtmlElement;
+    navbar?.classList.toggle('active');
 
-      gsap.registerPlugin(TweenMax)
+    gsap.registerPlugin(TweenMax);
 
-      // morphing the burger butto to be a close button
-      if (navbar?.classList.contains('active')) {
-        TweenMax.fromTo('.active',0.2,
-          {
-            height: 80,
-            ease:Expo.easeInOut,
-          },
-          {
-            height: "100vh",
-            ease:Expo.easeInOut,
-          }
-        )
-        main!.style.pointerEvents = "none";
-        setTimeout(()=> {
-        body!.style.height = "100%"
-        body!.style.overflow = "hidden"
-        },100)
-
-      } else {
-        main!.style.pointerEvents = "auto";
-        body!.style.height = "100%"
-        body!.style.overflow = "hidden"
-
-        TweenMax.fromTo('.mobile',0.2,
-          {
-            height: window.innerHeight,
-            ease:Expo.easeInOut,
-          },
-          {
-            height: 80,
-            ease:Expo.easeInOut,
-          }
-        )
-        navbar!.style.height = "auto";
-
-      }
-      
-
-
+    // morphing the burger butto to be a close button
+    if (navbar?.classList.contains('active')) {
+      TweenMax.fromTo(
+        '.active',
+        0.2,
+        {
+          height: 80,
+          ease: Expo.easeInOut,
+        },
+        {
+          height: '100vh',
+          ease: Expo.easeInOut,
+        }
+      );
+      main!.style.pointerEvents = 'none';
+      setTimeout(() => {
+        body!.style.height = '100%';
+        body!.style.overflow = 'hidden';
+      }, 100);
+    } else {
+      TweenMax.fromTo(
+        '.mobile',
+        0.2,
+        {
+          height: window.innerHeight,
+          ease: Expo.easeInOut,
+        },
+        {
+          height: 80,
+          ease: Expo.easeInOut,
+        }
+      );
+      main!.style.pointerEvents = 'auto';
+      body!.style.height = 'auto';
+      body!.style.overflow = 'auto';
+      navbar!.style.height = 'auto';
+    }
   }
-
-
 }
